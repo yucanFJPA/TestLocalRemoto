691cfac16d052ed47e886cb2751e027b89c68774
Francisco Jesús Pérez Aguilera <fjpa@gmv.com>
2024-01-12 23:24:49 +0100

    Botón

--- TLR/Form1.cs
+++ TLR/Form1.cs
@@ -16,5 +16,10 @@ namespace TLR
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("HOLA");
+        }
     }
 }
